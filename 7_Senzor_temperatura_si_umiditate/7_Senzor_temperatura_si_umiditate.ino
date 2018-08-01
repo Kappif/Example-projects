173d78aacfe92d36e761a8522d0c30e9c0e01045
Alex <alexxm1710@gmail.com>
2018-08-01 18:42:14 +0300

    Adds DHT project

---
+++ 7_Senzor_temperatura_si_umiditate/7_Senzor_temperatura_si_umiditate.ino
@@ -0,0 +1,19 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN PD7 //Ataseaza senzorul de pinul 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperatura = "); //Afiseaza temperatura prin portul serial
+  Serial.println(DHT.temperature);
+  Serial.print("Umiditate = "); //Afiseaza umiditatea prin portul serial
+  Serial.println(DHT.humidity);
+  delay(1000);
+}
